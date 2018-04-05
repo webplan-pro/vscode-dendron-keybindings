2df178386381c4da6bb29dba48f4610cf2840742
Till Salinger <t-tisali@microsoft.com>
2018-04-05 18:52:24 +0200

    bugfix: tests are now independent of existing settings

--- src/test/testData.ts
+++ src/test/testData.ts
@@ -1,6 +1,23 @@
 export const sublimeSettings = {
-    "draw_white_space": "selection",
-    "word_separators": "-/_",
-    "tab_size": 12,
-    "ensure_newline_at_eof_on_save": false
-}
+    "draw_white_space$test": "selection",
+    "word_separators$test": "-/_",
+    "tab_size$test": 12,
+    "ensure_newline_at_eof_on_save$test": false,
+};
+
+export const testMappings = {
+    "tab_size$test": "editor.tabSize$test", // Number
+    "word_separators$test": "editor.wordSeparators$test", // String
+    "ensure_newline_at_eof_on_save$test": "files.insertFinalNewline$test", // Boolean
+    "draw_white_space$test": {
+        "all": {
+            "editor.renderWhitespace$test": "all"
+        },
+        "none": {
+            "editor.renderWhitespace$test": "none"
+        },
+        "selection": {
+            "editor.renderWhitespace$test": "boundary"
+        },
+    },
+};
